e208caa42aaa90835dc7b0a04effe582e5868cfe
GiovanediPaula <giovanedipaula@gmail.com>
2020-09-30 11:49:12 -0300

    Mensagem Bom dia, Boa tarde e Boa noite

--- src/application/ProgramHelloWord.java
+++ src/application/ProgramHelloWord.java
@@ -5,6 +5,9 @@ public class ProgramHelloWord {
 	public static void main(String[] args) {
 
 		System.out.println("Hello, World!");
+		System.out.println("Bom dia!");
+		System.out.println("Boa tarde!");
+		System.out.println("Boa noite!");
 
 	}
 
